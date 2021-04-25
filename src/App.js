d39cce1742a8384ac3b1f221083932a652fca50c
Ahmedayadi91 <ahmedayadi91@gmail.com>
2021-04-25 15:19:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import  ImageInPublic from '../public/imageInPublic.png'
 import  ImageInSrc  from './imageInSrc.png'
+import  video1 from './video1.mp4'
+
 import './App.css';
 import Footer from './componant/footer'
 function App() {
@@ -10,12 +11,12 @@ function App() {
         <h2 className="title2"> Bienvenue dans mon premier web site </h2>
         <br />
         <div className="img1">
-          <img   src={ImageInPublic}    alt='image 1'   />
+          <img   src="/ImageInPublic.png"    alt='image 1'   />
           <img   src={ImageInSrc}    alt='image 2'   />
-         {/* <img 
+         <img 
           src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1200px-Visual_Studio_Code_1.35_icon.svg.png" width='100px ' height='100px'
           alt="new"
-          /> */}
+          />
         </div>
         
       <br/> <hr/>
@@ -24,25 +25,19 @@ function App() {
             src="https://www.entreprises-magazine.com/wp-content/uploads/2020/05/Gomycode-et-Tunisia-Jobs-696x385.jpg" width='400px ' height='200px'
              alt="new"
       /> <br/>
-      <video width="750" height="500" controls >
-      <source src="https://www.youtube.com/watch?v=Rq5SEhs9lws&ab_channel=Skillthrive" type="video/mp4"/>
-     </video>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/O6P86uwfdR0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
         
-
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/BURRD_nWJh0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-
-            {/* <video width="320" height="240" controls>
-
-      <source src="myVideo.mp4" type="video/mp4" />
-
-            </video> */}
-            <video className="video-container video-container-overlay" autoplay="" loop="" muted="">
-                   <source type="mp4" data-reactid=".0.1.0.0.0" src="https://www.youtube.com/watch?v=OD3F7J2PeYU"/>
+            <video width="520" height="400" controls>
+               <source src={video1} type="video/mp4" />
             </video>
-            <video>
-            <source src="https://www.youtube.com/watch?v=OD3F7J2PeYU"/>
-            </video>
+
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/fCZVL_8D048" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              {/* <video width="420px" height="440px">
+                <source src="https://www.w3schools.com/html/mov_bbb.mp4"  />
+              </video> */}
            <div> <Footer/> </div> 
     </div>
   );
